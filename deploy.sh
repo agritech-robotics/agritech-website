dbef4ee3c001ab2a44bc019fdd5559f64be0978f
nubonetics <45704452+nubonetics@users.noreply.github.com>
2019-02-04 17:16:40 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
-rm -rf public/
-HUGO_ENV="production" hugo --gc || exit 1
+rm -rf public
+git clone --depth 1 https://nubonetics:$GITHUB_ACCESS_TOKEN@github.com/agritech-robotics/agritech-robotics.github.io.git public
+hugo --config config.toml
 cd public
+git config user.email "nubonetics@gmail.com"
+git config --global user.name "nubonetics"
+git add -A
+git commit -m "Build from $CI_SERVER_NAME $CI_PIPELINE_ID"
+git push
